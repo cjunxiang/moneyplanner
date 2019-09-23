767e84895ca3b0541cdc067db7c0911c2419944e
cjunxiang <e0176013@u.nus.edu>
2019-09-24 00:01:06 +0800

    database methods for Events

--- BackEnd/app/database/index.js
+++ BackEnd/app/database/index.js
@@ -148,6 +148,190 @@ const deleteWalletByWalletId = walletId => {
   });
 };
 
+/**
+ * @param WalletId
+ * @returns echoes event list
+ */
+const fetchEventListByWalletId = walletId => {
+  logger.debug('fetchEventListByWalletId method started.');
+  return new Promise((resolve, reject) => {
+    request.post(
+      `${databaseURL}/event/fetch-event-list-by-wallet-id`,
+      {
+        json: {
+          WalletId: walletId
+        }
+      },
+      (error, res, body) => {
+        if (error) {
+          logger.error(
+            `Error ${error} occurred when fetching event list from wallet Id ${walletId}`
+          );
+          reject();
+        }
+        resolve(body);
+      }
+    );
+  });
+};
+
+/**
+ * @param WalletId
+ * @param eventObject={walletId, InflowOrOutflow, name, price, date, remarks}
+ * @returns echoes added event
+ */
+const addEventByWalletId = (walletId, eventObject) => {
+  logger.debug('addEventByWalletId method started.');
+  return new Promise((resolve, reject) => {
+    request.post(
+      `${databaseURL}/event/add-event-by-wallet-id`,
+      {
+        json: {
+          WalletId: walletId,
+          EventName: eventObject.name,
+          InflowOrOutflow: eventObject.InflowOrOutflow,
+          Type: eventObject.type,
+          Name: eventObject.name,
+          Price: eventObject.price,
+          Date: eventObject.date,
+          Remarks: eventObject.remarks
+        }
+      },
+      (error, res, body) => {
+        if (error) {
+          logger.error(
+            `Error ${error} occurred when adding event to wallet Id ${walletId}`
+          );
+          reject();
+        }
+        resolve(body);
+      }
+    );
+  });
+};
+
+/**
+ * @param EventId
+ * @returns event object
+ */
+const fetchEventByEventId = eventId => {
+  logger.debug('fetchEventByEventId method started.');
+  return new Promise((resolve, reject) => {
+    request.post(
+      `${databaseURL}/event/fetch-event-by-event-id`,
+      {
+        json: {
+          EventId: eventId
+        }
+      },
+      (error, res, body) => {
+        if (error) {
+          logger.error(
+            `Error ${error} occurred when fetching event from eventId ${eventId}`
+          );
+          reject();
+        }
+        resolve(body);
+      }
+    );
+  });
+};
+
+/**
+ * @param eventId, neweventObject
+ * @returns echoes event object updated
+ */
+const editEventByEventId = (eventId, newEventObject) => {
+  logger.debug('editEventByEventId method started.');
+  return new Promise((resolve, reject) => {
+    request.post(
+      `${databaseURL}/event/edit-event-by-event-id`,
+      {
+        json: {
+          EventId: eventId,
+          EventName: newEventObject.name,
+          InflowOrOutflow: newEventObject.InflowOrOutflow,
+          Type: newEventObject.type,
+          Name: newEventObject.name,
+          Price: newEventObject.price,
+          Date: newEventObject.date,
+          Remarks: newEventObject.remarks
+        }
+      },
+      (error, res, body) => {
+        if (error) {
+          logger.error(
+            `Error ${error} occurred when editting event from eventId ${eventId}`
+          );
+          reject();
+        }
+        resolve(body);
+      }
+    );
+  });
+};
+
+/**
+ * @param eventId
+ * @returns echoes deleted object
+ */
+const deleteEventByEventId = eventId => {
+  logger.debug('deleteEventByEventId method started.');
+  return new Promise((resolve, reject) => {
+    request.post(
+      `${databaseURL}/event/delete-event-by-event-id`,
+      {
+        json: {
+          EventId: eventId
+        }
+      },
+      (error, res, body) => {
+        if (error) {
+          logger.error(
+            `Error ${error} occurred when deleting event, eventId ${eventId}`
+          );
+          reject();
+        }
+        resolve(body);
+      }
+    );
+  });
+};
+
+/**
+ * @param (eventName, walletId, userId)
+ * @returns echoes deleted object
+ */
+const getEventIdByEventNameAndWalletIdAndUserId = (
+  eventName,
+  walletId,
+  userId
+) => {
+  logger.debug('getEventIdByEventNameAndWalletIdAndUserId method started.');
+  return new Promise((resolve, reject) => {
+    request.post(
+      `${databaseURL}/event/get-event-id-by-event-name-and-wallet-id-and-user-id`,
+      {
+        json: {
+          walletId: walletId,
+          eventName: eventName,
+          userId: userId
+        }
+      },
+      (error, res, body) => {
+        if (error) {
+          logger.error(
+            `Error ${error} occurred when getting eventId with eventname` +
+              ` ${eventName} WalletId ${walletId} and UserId ${userId}`
+          );
+          reject();
+        }
+        resolve(body);
+      }
+    );
+  });
+};
+
 // Exports all methods
 module.exports = {
   //Below is for User
@@ -162,13 +346,13 @@ module.exports = {
   fetchWalletByWalletId,
   editWalletByWalletId,
   deleteWalletByWalletId,
-  getWalletIdByWalletNameAndUserId
+  getWalletIdByWalletNameAndUserId,
   //below is for Events
-  // fetchEventListByWalletId
-  // addEventByWalletId,
-  // fetchEventByEventId,
-  // editEventByEventId,
-  // deleteEventByEventId,
-  // getEventIdByEventNameAndWalletIdAndUserId
+  fetchEventListByWalletId,
+  addEventByWalletId,
+  fetchEventByEventId,
+  editEventByEventId,
+  deleteEventByEventId,
+  getEventIdByEventNameAndWalletIdAndUserId
   //below is others
 };
